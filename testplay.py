d282efaafb76e993d4d45e89058032a5a781022f
miracle <1021621765@qq.com>
2020-05-26 11:23:11 +0800

    demo

--- testplay.py
+++ testplay.py
@@ -1,6 +1,7 @@
 import _thread
 import time
 
+'''
 def print_thread( ThreadName, delay):
     count = 0
     while count < 5:
@@ -12,6 +13,47 @@ try:
     _thread.start_new_thread( print_thread, ("Thread-2",2,))
 except:
     print("Error:无法启动线程")
+    
 
 while 1:
     pass
+
+'''
+
+print("hello python")
+
+counter = 100  #整数型
+miles = 5.0    #浮点型变量
+name = "nancy"  #字符串
+
+print(counter)
+print(miles)
+print(name)
+
+
+x = input('请输入一个字符串')
+print(x[-1] + "宋永宇")
+
+
+a = 1
+b = 'python'
+c = ['1','2','3']
+
+str(a)
+
+print(a)
+print(b + '\nhello')
+
+print(c[1:3])
+
+d = {'a','b','c','d','e','f'}
+
+print(d)
+
+d.remove('c')
+print(d)
+
+dict = {'key1': 'name', 'value1': '张三', 'key2': 'sex', 'value2': '男'}
+print(dict)
+print(dict.keys())
+print(dict.values())
