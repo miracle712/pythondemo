513e426e8880fce04fe60c05dc52f5594b507de7
miracle <1021621765@qq.com>
2020-06-08 21:43:23 +0800

    demo

--- testplay.py
+++ testplay.py
@@ -1,5 +1,6 @@
 import _thread
 import time
+import isort
 
 '''
 def print_thread( ThreadName, delay):
@@ -19,7 +20,7 @@ while 1:
     pass
 
 '''
-
+'''
 print("hello python")
 
 counter = 100  #整数型
@@ -60,3 +61,53 @@ dict = {'key1': 'name', 'value1': '张三', 'key2': 'sex', 'value2': '男'}
 print(dict)
 print(dict.keys())
 print(dict.values())
+
+'''
+'''
+number = 7
+guess = 0
+print('数字猜谜游戏')
+while guess != number:
+    guess = int(input('请输入你猜的数字：'))
+    if guess == number:
+        print('恭喜你猜对了')
+    elif guess < number:
+        print('猜小了')
+    elif guess > number:
+        print('猜大了')
+'''
+'''
+a = 33
+b = 11
+c = 22
+t1 = 0 
+t2 = 0
+t3 = 0
+
+if b < c and b < a:
+    t1 = b
+    print('a=',b)
+if c > b and c < a:
+    t2 = c
+    print('b=',c)
+if a > b and a > c :
+    t3 = a
+    print('c=',a)
+
+'''
+
+'''
+n = 100
+sum = 0
+counter = 1
+while counter <= n:
+    sum = sum + counter
+    counter += 1
+    print('1到 %d 之和为：%d' % (n,sum))
+    print('多少次：',counter)
+'''
+
+for i in range(7):
+    print(" "*(6-i),end="")
+    print(" * "*(i+1))
+
